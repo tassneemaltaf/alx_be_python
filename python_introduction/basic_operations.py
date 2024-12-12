914bc0f3643c8bdef1a080faac48135c3db1b253
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-12-12 00:49:11 -0800

    First file added

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1, number2 = 10, 5
+addition = number1 + number2
+subtraction = number1 - number2
+mult = number1 * number2
+
+print("Addition of ", number1, " and ", number2, " is ", addition)
+print("Subtraction of ", number1, " and ", number2, " is ", subtraction)
+print("Multiplication of ", number1, " and ", number2, " is ", mult)
