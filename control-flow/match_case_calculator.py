8415a72e5f44f3bf68783563cd605ad673d3e321
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-12-22 12:35:05 -0800

    Added third task

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -2,13 +2,17 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
 
-if operation == "+":
-    print("The result is ", num1 + num2)
-elif operation == "-":
-    print("The result is ", num1 - num2)
-elif operation == "*":
-    print("The result is ", num1 * num2)
-elif operation == "/":
-    print("The result is ", num1 / num2)
-elif num1 == 0 or num2 == 0:
-    print("Cannot divide by zero")
+match operation:
+    case "+":
+        print("The result is ", num1 + num2)
+    case "-":
+        print("The result is ", num1 - num2)
+    case "*":
+        print("The result is ", num1 * num2)
+    case "/":
+        if num2 != 0:
+            print("The result is ", num1 / num2)
+        else:
+            print("Cannot divide by zero.")
+    case _:
+        print("Invalid operation")
---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,3 @@
+num = int(input("Enter a number to see its multiplication table: "))
+for i in range(1, 11):
+        print(num, " * ", i, " = ", num * i)
