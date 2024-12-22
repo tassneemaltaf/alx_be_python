b229c45677ae31ac9fbac3d486c8988b06fb714b
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-12-22 12:08:06 -0800

    Edited first task

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
-weather_input = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
-if weather_input == "sunny":
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif weather_input == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif weather_input == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
