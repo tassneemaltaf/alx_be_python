9595d226c39f2fa7d116b39b794913d2a3e1a24a
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-01-17 06:51:57 -0800

    Second task added

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,30 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            new_item = input("Enter the item name: ")
+            shopping_list.append(new_item)
+        elif choice == '2':
+            delete_item = input("Enter the name of the item you wish to delete: ")
+            shopping_list.remove(delete_item)
+        elif choice == '3':
+            for item in shopping_list:
+                print(item)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
