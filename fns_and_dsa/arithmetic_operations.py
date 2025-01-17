fc90b5e755d5cdda7ae8ac2188142513a1607fb3
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-01-17 06:41:36 -0800

    Task 1 edited

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,9 +6,9 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
-        if num1 != 0:
-            return num1 / num2
+        if num2 == 0:
+            return "Cannot divide by 0"
         else:
-            return "cannot divide by 0"
+            return num1 / num2
     else:
         return "error"
